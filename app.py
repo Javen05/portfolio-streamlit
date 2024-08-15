0e77211852ad2236d19a01b0f58e48cc8447bab7
Javen <107395637+Javen05@users.noreply.github.com>
2024-08-15 20:31:52 +0800

    seperated variables

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+### Libraries ###
 import streamlit as st
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import datetime
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+
+### Personal Links ###
+from config import LINKS
 
 ### Components ###
 def card_component(title, text=None, icon=None, url=None):
@@ -85,6 +90,14 @@ def display_events(items):
                 for task in item["tasks"]:
                     st.write(f"- {task}")
 
+
+### Function to load data from CSV ###
+def load_skills_csv(file_path):
+    df = pd.read_csv(file_path, encoding='utf-8')
+    skills_freq = dict(zip(df['Skill'], df['Frequency']))
+    return skills_freq
+
+
 ### Sidebar Navigation ###
 st.sidebar.title("Navigation")
 pages = ["About Me", "Documents", "Projects"]
@@ -94,7 +107,7 @@ selected_page = st.sidebar.selectbox("Go to", pages)
 ### Pages ###
 if selected_page == "About Me":
     st.header("Profile")
-    MY_GITHUB_URL = "https://raw.githubusercontent.com/Javen05/Javen05/main/README.md"
+    from config import MY_GITHUB_URL, PROFILE_PICTURE_URL
     profile_info = fetch_info(MY_GITHUB_URL)
     # Calculate the age
     current_year = datetime.now().year
@@ -102,7 +115,7 @@ if selected_page == "About Me":
 
     card_component(profile_info[0],
                     f'🇸🇬<br>Male<br>{age} y/o', 
-                   icon="https://avatars.githubusercontent.com/u/107395637?s=400&u=0b7ac436dfb509803792053287d0f5dcfa986982&v=4")
+                   icon=PROFILE_PICTURE_URL)
     
     card_component("About Me",
                    text=profile_info[3])
@@ -111,218 +124,15 @@ if selected_page == "About Me":
                    text="<br>".join(profile_info[11:]))
 
     st.header("Skills")
-    skills_freq = {
-        'Python': 81,
-        'SQL': 80,
-        'Tableau': 79,
-        'Powerpoint': 49,
-        'Canva': 47,
-        'Machine Learning': 74,
-        'Data Storytelling': 75,
-        'Data Mining': 66,
-        'Analytical': 75,
-        'Data Cleaning': 69,
-        'Excel': 66,
-        'Problem-Solving': 80,
-        'Critical Thinking': 75,
-        'Communication Skills': 62,
-        'Data Warehousing': 78,
-        'Relational Database': 69,
-        'ETL': 72,
-        'Data Engineering': 70,
-        'Big Data': 65,
-        'Data Modeling': 65,
-        'Programming': 70,
-        'Data Pipeline': 74,
-        'BI': 75,
-        'RPA': 67,
-        'Full Stack': 68,
-        'Creativity': 70,
-    }
-
+    skills_freq = load_skills_csv("skills.csv")
     wordcloud = WordCloud().generate_from_frequencies(skills_freq)
     plt.figure(figsize=(20, 10))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     st.pyplot(plt)
 
-    items = [
-        {
-            "start": "2024-07-27",
-            "end": "",
-            "content": "Shake Shack Crew (PT)",
-            "skills": [
-            "Food Safety Certification (Level 1)"
-            ],
-        },
-        {
-            "start": "2024-04-15",
-            "end": "2024-10-16",
-            "content": "RPA Developer Internship @ Synapxe",
-            "skills": [],
-        },
-        {
-            "start": "2023-04-01",
-            "end": "2024-04-01",
-            "content": "Y2 Diploma",
-            "events": [
-                "Participated in Alibaba Cloud AI Hackathon",
-                "Participated in InnoCon 2023 - Hackathon by Lexagle",
-                "Participated in PSA Hackathon 2023 - CodeSprint",
-                "Participated in Tableau Data + Movies Hackathon (Within TP)",
-                "Completed TP Lead Programme"
-            ],
-            "skills": [
-                "Engineered Machine Learning models and extracted insights by interpreting SHAP",
-                "Developed supervised and unsupervised models using Python libraries (Scikit-learn, Pandas, Numpy, Matplotlib, Seaborn, etc.)",
-                "Constructed Data Storyboards using Tableau",
-                "Conducted Data Mining and Business Analytics using SAS Enterprise Miner",
-                "Designed a Data Warehouse and used it for conducting Business Intelligence",
-                "Developed an automated ETL Pipeline using Alteryx OR AWS",
-                "Data cleaning and transformations using Python"
-            ],
-            "achievements": [
-                "Director's List Award (AY2023/2024)",
-                "Champion for Sustainability Campaign Design Competition - 'Reduce Single-Use Plastics' (Within TP)",
-                "Bronze Award for SUSS Analytics and Visualisation Challenge 2023",
-            ]
-        },
-        {
-            "start": "2022-04-01",
-            "end": "2023-04-01",
-            "content": "Y1 Diploma",
-            "events": [
-                "Trained for WorldSkills SG IT Software Solutions for Business for 1 Semester",
-                "Participated in Sustainability Innovation Challenge 2023 by Junior Achievement Singapore",
-                "Attended an AWS workshop",
-                "Attended an RPA seminar",
-            ],
-            "skills": [
-                "Full Stack Development using MySQL, Express, React, NodeJS",
-                "App Ideation and Wireframing using Adobe XD",
-                "Built a hardcoded Music App on Android Studio using Java",
-                "Built Dashboards using PowerBi",
-                "Built Data Preparation Pipelines using KNIME"
-            ],
-            "achievements": [
-                "Director's List Award (AY2022/2023), 4.0 GPA for Y1",
-                "Participation (58/87) - Lag and Crash 3.0 (CTF)",
-                "Bronze Medal - Poly-ITE Olympiad for Informatic (PIOI)"
-            ]
-        },
-        {
-            "start": "2021-01-01",
-            "end": "2022-01-01",
-            "content": "Sec 4",
-            "events": [
-                "Participated in Shell Education NXplorers"
-            ]
-        },
-        {
-            "start": "2020-01-01",
-            "end": "2021-01-01",
-            "content": "Sec 3",
-            "events": [
-                "Micro:Bit Training - Innovation Garage",
-                "Organisation of CCA Display Day"
-            ]
-        },
-        {
-            "start": "2019-01-01",
-            "end": "2020-01-01",
-            "content": "Sec 2",
-            "events": [
-                "Drone Swarming DJI Tello EDU - 65DRONES",
-                "Participation - Singapore Amazing Flying Machine Competition (SAFMC)"
-            ]
-        },
-        {
-            "start": "2018-01-01",
-            "end": "2019-01-01",
-            "content": "Sec 1",
-            "events": [
-                "Participation - Science & Technology Challenge by NYP",
-                "Certificate of Appreciation - Maker Faire Singapore"
-            ]
-        },
-        {
-            "start": "2016",
-            "end": "2017",
-            "content": "Pri 6",
-            "achievements": [
-                "SYF Certificate of Recognition - Art Exhibition",
-                "EAGLES (Edusave Award for Achievement, Good Leadership and Services)",
-                "Edusave Certificate of Academic Achievement"
-            ]
-        },
-        {
-            "start": "2014",
-            "end": "2015",
-            "content": "Pri 4",
-            "achievements": [
-                "Edusave Certificate of Academic Achievement (Top 25%)",
-                "Edusave Good Progress Award"
-            ]
-        },
-        {
-            "start": "2012",
-            "end": "2013",
-            "content": "Pri 2",
-            "achievements": [
-                "Edusave Certificate of Academic Achievement (Top 25%)",
-                "Edusave Good Progress Award",
-                "Good Progress Award for Chinese Language"
-            ]
-        },
-        {
-            "start": "2023-03-05",
-            "end": "2023-04-08",
-            "content": "Warehouse Assistant @ UPS Supply Chain Solutions",
-            "tasks": [
-                "Performed Data Entry using SAP Hana Inventory Management System",
-                "Produced Excel Spreadsheets from transaction invoices",
-                "Assisted with the logistics of products"
-            ]
-        },
-        {
-            "start": "2022-10-01",
-            "end": "2022-10-31",
-            "content": "Grocery Shopper @ NTUC FairPrice",
-            "tasks": [
-                "Diligently picked and scanned products for online orders",
-                "Assembled and packed groceries for shipping",
-                "Ensured positive customer experience by assisting with their requests"
-            ]
-        },
-        {
-            "start": "2022-09-01",
-            "end": "2022-09-30",
-            "content": "Logistics Assistant @ KrisShop",
-            "tasks": [
-                "Prepared carts for next flight by restocking and updating stock status using a scanner"
-            ]
-        },
-        {
-            "start": "2022-02-01",
-            "end": "2022-02-28",
-            "content": "Picker @ Omni Logistics",
-            "tasks": [
-                "Picked products specified on a list in a warehouse"
-            ]
-        },
-        {
-            "start": "2021-11-15",
-            "end": "2021-11-30",
-            "content": "Replenisher @ NTUC FairPrice",
-            "tasks": [
-                "Restocked inventory in storage",
-                "Replenished shelves in the frozen food section",
-                "Assisted customers with their queries"
-            ]
-        }
-    ]
-
     st.header('Progression')
+    from items import items
     display_events(items)
 
     st.header('LeetCode Profile')
@@ -349,23 +159,23 @@ if selected_page == "About Me":
 
     st.header('Contact Information')
     card_component("Contact me via email",
-                     text="javenlai5@gmail.com",
-                     url="mailto:javenlai5@gmail.com?subject=Feedback&body=Hi Javen,")
+                     text=LINKS["email"]["text"],
+                     url=LINKS["email"]["url"])
 
     card_component("Connect on LinkedIn",
-                     url="https://www.linkedin.com/in/javen-lai/",
-                     icon="https://media.licdn.com/dms/image/D5603AQH9NtyrrcT2dA/profile-displayphoto-shrink_100_100/0/1695917340965?e=1720051200&v=beta&t=nyf3ufspWb3NPxk_z-IKVocksabcVTltOfAMiAS8lJs")
+                    url=LINKS["linkedin"]["url"],
+                    icon=LINKS["linkedin"]["icon"])
     
     card_component("Check out my Github",
-                        url="github.com/Javen05",
-                        icon="https://img.icons8.com/?size=256&id=g7P0iny5Rros&format=png")
+                    url=LINKS["github"]["url"],
+                    icon=LINKS["github"]["icon"])
 
 elif selected_page == "Documents":
     st.header("Documents")
-    resume_url = "https://drive.google.com/file/d/1Oe-QSj_f-azkXOviGjzVmZtPibsxgPkq/preview"
+    resume_url = LINKS["resume"]["url"]
     card_component("Resume", url=resume_url, text=f'<iframe src="{resume_url}" width="100%" height="800px"></iframe>')
 
-    poly_url = "https://drive.google.com/file/d/1Pi4CJmaFXUtuAmPIp-oxsRdQ30P9xwdA/preview"
+    poly_url = LINKS["polytechnic_transcript"]["url"]
     card_component("Polytechnic Transcript", url=poly_url, text=f'<iframe src="{poly_url}" width="100%" height="800px"></iframe>')
    
 
@@ -377,4 +187,4 @@ elif selected_page == "Projects":
     for repo in repositories_info:
         if repo["info"]:
             if search_query.lower() in repo["name"].lower() or search_query.lower() in repo["info"].lower():
-                card_component(title=repo["name"], text=repo["info"], url=repo["url"], icon="https://img.icons8.com/?size=256&id=g7P0iny5Rros&format=png")
+                card_component(title=repo["name"], text=repo["info"], url=repo["url"], icon=LINKS["github"]["icon"])
