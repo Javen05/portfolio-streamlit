55f032686d1f732bcdd459299244277aa3488d01
Javen <107395637+Javen05@users.noreply.github.com>
2024-08-15 20:32:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+MY_GITHUB_URL = "https://raw.githubusercontent.com/Javen05/Javen05/main/README.md"
+PROFILE_PICTURE_URL = "https://avatars.githubusercontent.com/u/107395637?s=400&u=0b7ac436dfb509803792053287d0f5dcfa986982&v=4"
+LINKS = {
+    "email": {
+        "text": "javenlai5@gmail.com",
+        "url": "mailto:javenlai5@gmail.com?subject=Feedback&body=Hi Javen,"
+    },
+    "linkedin": {
+        "url": "https://www.linkedin.com/in/javen-lai/",
+        "icon": "https://media.licdn.com/dms/image/D5603AQH9NtyrrcT2dA/profile-displayphoto-shrink_100_100/0/1695917340965?e=1720051200&v=beta&t=nyf3ufspWb3NPxk_z-IKVocksabcVTltOfAMiAS8lJs"
+    },
+    "github": {
+        "url": "https://github.com/Javen05",
+        "icon": "https://img.icons8.com/?size=256&id=g7P0iny5Rros&format=png"
+    },
+    "resume": {
+        "url": "https://drive.google.com/file/d/1Oe-QSj_f-azkXOviGjzVmZtPibsxgPkq/preview"
+    },
+    "polytechnic_transcript": {
+        "url": "https://drive.google.com/file/d/1Pi4CJmaFXUtuAmPIp-oxsRdQ30P9xwdA/preview"
+    }
+}
---
+++ items.py
@@ -0,0 +1,175 @@
+items = [
+    {
+        "start": "2024-07-27",
+        "end": "",
+        "content": "Shake Shack Crew (PT)",
+        "skills": [
+        "Food Safety Certification (Level 1)"
+        ],
+    },
+    {
+        "start": "2024-04-15",
+        "end": "2024-10-16",
+        "content": "RPA Developer Internship @ Synapxe",
+        "skills": [],
+    },
+    {
+        "start": "2023-04-01",
+        "end": "2024-04-01",
+        "content": "Y2 Diploma",
+        "events": [
+            "Participated in Alibaba Cloud AI Hackathon",
+            "Participated in InnoCon 2023 - Hackathon by Lexagle",
+            "Participated in PSA Hackathon 2023 - CodeSprint",
+            "Participated in Tableau Data + Movies Hackathon (Within TP)",
+            "Completed TP Lead Programme"
+        ],
+        "skills": [
+            "Engineered Machine Learning models and extracted insights by interpreting SHAP",
+            "Developed supervised and unsupervised models using Python libraries (Scikit-learn, Pandas, Numpy, Matplotlib, Seaborn, etc.)",
+            "Constructed Data Storyboards using Tableau",
+            "Conducted Data Mining and Business Analytics using SAS Enterprise Miner",
+            "Designed a Data Warehouse and used it for conducting Business Intelligence",
+            "Developed an automated ETL Pipeline using Alteryx OR AWS",
+            "Data cleaning and transformations using Python"
+        ],
+        "achievements": [
+            "Director's List Award (AY2023/2024)",
+            "Champion for Sustainability Campaign Design Competition - 'Reduce Single-Use Plastics' (Within TP)",
+            "Bronze Award for SUSS Analytics and Visualisation Challenge 2023",
+        ]
+    },
+    {
+        "start": "2022-04-01",
+        "end": "2023-04-01",
+        "content": "Y1 Diploma",
+        "events": [
+            "Trained for WorldSkills SG IT Software Solutions for Business for 1 Semester",
+            "Participated in Sustainability Innovation Challenge 2023 by Junior Achievement Singapore",
+            "Attended an AWS workshop",
+            "Attended an RPA seminar",
+        ],
+        "skills": [
+            "Full Stack Development using MySQL, Express, React, NodeJS",
+            "App Ideation and Wireframing using Adobe XD",
+            "Built a hardcoded Music App on Android Studio using Java",
+            "Built Dashboards using PowerBi",
+            "Built Data Preparation Pipelines using KNIME"
+        ],
+        "achievements": [
+            "Director's List Award (AY2022/2023), 4.0 GPA for Y1",
+            "Participation (58/87) - Lag and Crash 3.0 (CTF)",
+            "Bronze Medal - Poly-ITE Olympiad for Informatic (PIOI)"
+        ]
+    },
+    {
+        "start": "2021-01-01",
+        "end": "2022-01-01",
+        "content": "Sec 4",
+        "events": [
+            "Participated in Shell Education NXplorers"
+        ]
+    },
+    {
+        "start": "2020-01-01",
+        "end": "2021-01-01",
+        "content": "Sec 3",
+        "events": [
+            "Micro:Bit Training - Innovation Garage",
+            "Organisation of CCA Display Day"
+        ]
+    },
+    {
+        "start": "2019-01-01",
+        "end": "2020-01-01",
+        "content": "Sec 2",
+        "events": [
+            "Drone Swarming DJI Tello EDU - 65DRONES",
+            "Participation - Singapore Amazing Flying Machine Competition (SAFMC)"
+        ]
+    },
+    {
+        "start": "2018-01-01",
+        "end": "2019-01-01",
+        "content": "Sec 1",
+        "events": [
+            "Participation - Science & Technology Challenge by NYP",
+            "Certificate of Appreciation - Maker Faire Singapore"
+        ]
+    },
+    {
+        "start": "2016",
+        "end": "2017",
+        "content": "Pri 6",
+        "achievements": [
+            "SYF Certificate of Recognition - Art Exhibition",
+            "EAGLES (Edusave Award for Achievement, Good Leadership and Services)",
+            "Edusave Certificate of Academic Achievement"
+        ]
+    },
+    {
+        "start": "2014",
+        "end": "2015",
+        "content": "Pri 4",
+        "achievements": [
+            "Edusave Certificate of Academic Achievement (Top 25%)",
+            "Edusave Good Progress Award"
+        ]
+    },
+    {
+        "start": "2012",
+        "end": "2013",
+        "content": "Pri 2",
+        "achievements": [
+            "Edusave Certificate of Academic Achievement (Top 25%)",
+            "Edusave Good Progress Award",
+            "Good Progress Award for Chinese Language"
+        ]
+    },
+    {
+        "start": "2023-03-05",
+        "end": "2023-04-08",
+        "content": "Warehouse Assistant @ UPS Supply Chain Solutions",
+        "tasks": [
+            "Performed Data Entry using SAP Hana Inventory Management System",
+            "Produced Excel Spreadsheets from transaction invoices",
+            "Assisted with the logistics of products"
+        ]
+    },
+    {
+        "start": "2022-10-01",
+        "end": "2022-10-31",
+        "content": "Grocery Shopper @ NTUC FairPrice",
+        "tasks": [
+            "Diligently picked and scanned products for online orders",
+            "Assembled and packed groceries for shipping",
+            "Ensured positive customer experience by assisting with their requests"
+        ]
+    },
+    {
+        "start": "2022-09-01",
+        "end": "2022-09-30",
+        "content": "Logistics Assistant @ KrisShop",
+        "tasks": [
+            "Prepared carts for next flight by restocking and updating stock status using a scanner"
+        ]
+    },
+    {
+        "start": "2022-02-01",
+        "end": "2022-02-28",
+        "content": "Picker @ Omni Logistics",
+        "tasks": [
+            "Picked products specified on a list in a warehouse"
+        ]
+    },
+    {
+        "start": "2021-11-15",
+        "end": "2021-11-30",
+        "content": "Replenisher @ NTUC FairPrice",
+        "tasks": [
+            "Restocked inventory in storage",
+            "Replenished shelves in the frozen food section",
+            "Assisted customers with their queries"
+        ]
+    }
+]
